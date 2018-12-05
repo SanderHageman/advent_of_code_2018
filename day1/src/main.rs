5d909d3e4022efade8da24ea57f7ff387d9faf08
Sander <sander@paladinstudios.com>
2018-12-05 13:14:50 +0100

    ADD part two of day one

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,40 @@
+use std::collections::HashSet;
 use std::io::{self, BufRead};
 
 fn main() {
     let input = io::stdin();
-    let mut result = 0;
+    let mut frequencies = HashSet::new();
+    let mut first_result = 0;
+    let mut t_freq = 0;
+    let mut changes: Vec<i32> = Vec::new();
+    let mut second_result: Option<i32> = None;
+
     for line in input.lock().lines() {
-        let mut line_str = line.unwrap_or_default();
-        result += line_str.parse::<i32>().unwrap_or_default();
+        let line_str = line.unwrap_or_default();
+        let change = line_str.parse::<i32>().unwrap_or_default();
+
+        first_result += change;
+        changes.push(change);
     }
-    println!("Result Day1: {:?}", result);
+
+    if second_result.is_none() {
+        loop {
+            for _change in changes.iter() {
+                t_freq += _change;
+                if !frequencies.insert(t_freq) {
+                    if second_result.is_none() {
+                        second_result = Some(t_freq);
+                        break;
+                    }
+                }
+            }
+
+            if second_result.is_some() {
+                break;
+            }
+        }
+    }
+
+    println!("Result Day1_1: {:?}", first_result);
+    println!("Result Day1_2: {:?}", second_result);
 }
