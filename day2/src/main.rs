33fbb21d278830c9bde8e47650e2ad98222ee446
Sander <sander@paladinstudios.com>
2018-12-06 12:25:57 +0100

    Move gitignore

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let input = io::stdin();
+    let mut first_result = 0;
+
+    for line in input.lock().lines() {
+        let line_str = line.unwrap_or_default();
+        println!("{}", line_str);
+    }
 }
